28befff6a90dbb97e5ae58c50e85e9f1158bbc96
KashenkovSergej <kashenkovsergej@yandex.ru>
2023-02-15 14:15:10 +0300

    Create application

---
+++ src/test/java/com/raccoondev/flashcards/FlashCardsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.raccoondev.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
