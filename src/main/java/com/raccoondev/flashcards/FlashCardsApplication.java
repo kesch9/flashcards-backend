ae578008cfda874d2d6e89b4c7457321f1fb94ad
KashenkovSergej <kashenkovsergej@yandex.ru>
2023-04-07 11:25:06 +0300

    fix datasource auto config

--- src/main/java/com/raccoondev/flashcards/FlashCardsApplication.java
+++ src/main/java/com/raccoondev/flashcards/FlashCardsApplication.java
@@ -3,8 +3,9 @@ package com.raccoondev.flashcards;
 import de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {EmbeddedMongoAutoConfiguration.class})
+@SpringBootApplication(exclude = {EmbeddedMongoAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class FlashCardsApplication {
 
     public static void main(String[] args) {
